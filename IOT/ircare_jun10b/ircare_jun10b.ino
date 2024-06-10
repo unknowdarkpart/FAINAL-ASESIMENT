aebbf9b2fd32552f456969d064d3ace2483a33c0
unknowdarkpart <unknowdarkpart@gmail.com>
2024-06-11 01:36:01 +0300

    Add files via upload

---
+++ IOT/ircare_jun10b/ircare_jun10b.ino
@@ -0,0 +1,74 @@
+#define LEFT_SENSOR_PIN 2
+#define RIGHT_SENSOR_PIN 3
+//a
+#define LEFT_MOTOR_PIN1 4
+#define LEFT_MOTOR_PIN2 5
+//b
+#define RIGHT_MOTOR_PIN1 6
+#define RIGHT_MOTOR_PIN2 7
+
+void setup() {
+  // Initialize sensor pins
+  pinMode(LEFT_SENSOR_PIN, INPUT);
+  pinMode(RIGHT_SENSOR_PIN, INPUT);
+  
+  // Initialize motor pins
+  pinMode(LEFT_MOTOR_PIN1, OUTPUT);
+  pinMode(LEFT_MOTOR_PIN2, OUTPUT);
+  pinMode(RIGHT_MOTOR_PIN1, OUTPUT);
+  pinMode(RIGHT_MOTOR_PIN2, OUTPUT);
+  
+  // Start with motors stopped
+  stopMotors();
+}
+
+void loop() {
+  // Read sensor values
+  int leftSensorValue = digitalRead(LEFT_SENSOR_PIN);
+  int rightSensorValue = digitalRead(RIGHT_SENSOR_PIN);
+
+  // Both sensors on white surface, move forward
+  if (leftSensorValue == HIGH && rightSensorValue == HIGH) {
+    moveForward();
+  }
+  // Left sensor on black line, turn left
+  else if (leftSensorValue == LOW && rightSensorValue == HIGH) {
+    turnLeft();
+  }
+  // Right sensor on black line, turn right
+  else if (leftSensorValue == HIGH && rightSensorValue == LOW) {
+    turnRight();
+  }
+  // Both sensors on black line, stop (could be an intersection or end of line)
+  else {
+    stopMotors();
+  }
+}
+
+void moveForward() {
+  digitalWrite(LEFT_MOTOR_PIN1, HIGH);
+  digitalWrite(LEFT_MOTOR_PIN2, LOW);
+  digitalWrite(RIGHT_MOTOR_PIN1, HIGH);
+  digitalWrite(RIGHT_MOTOR_PIN2, LOW);
+}
+
+void turnLeft() {
+  digitalWrite(LEFT_MOTOR_PIN1, LOW);
+  digitalWrite(LEFT_MOTOR_PIN2, HIGH);
+  digitalWrite(RIGHT_MOTOR_PIN1, HIGH);
+  digitalWrite(RIGHT_MOTOR_PIN2, LOW);
+}
+
+void turnRight() {
+  digitalWrite(LEFT_MOTOR_PIN1, HIGH);
+  digitalWrite(LEFT_MOTOR_PIN2, LOW);
+  digitalWrite(RIGHT_MOTOR_PIN1, LOW);
+  digitalWrite(RIGHT_MOTOR_PIN2, HIGH);
+}
+
+void stopMotors() {
+  digitalWrite(LEFT_MOTOR_PIN1, LOW);
+  digitalWrite(LEFT_MOTOR_PIN2, LOW);
+  digitalWrite(RIGHT_MOTOR_PIN1, LOW);
+  digitalWrite(RIGHT_MOTOR_PIN2, LOW);
+}
